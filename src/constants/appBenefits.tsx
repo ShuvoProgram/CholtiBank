1226441857d86678f100ac9259978090a7c3a6d0
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 13:45:35 +0600

    Add app benefits icons

---
+++ src/constants/appBenefits.tsx
@@ -0,0 +1,39 @@
+import { GiOverInfinity } from "react-icons/gi";
+import {
+  FaGlobe,
+  FaShieldAlt,
+  FaExchangeAlt,
+  FaClock,
+  FaPercentage,
+  FaMobileAlt,
+  FaBell,
+  FaChartLine,
+  FaMoneyCheckAlt,
+  FaTools,
+  FaLightbulb,
+  FaGift,
+  FaMobile,
+  FaFileAlt,
+  FaQuestionCircle,
+} from "react-icons/fa";
+
+
+
+export const appBenefits = [
+    { icon: <GiOverInfinity className="md:text-2xl text-blue-500" />, title: "Seamless Banking Experience" },
+    { icon: <FaGlobe className="md:text-2xl text-green-500" />, title: "MultiCurrency Support" },
+    { icon: <FaShieldAlt className="md:text-2xl text-purple-500" />, title: "Cutting-Edge Security" },
+    { icon: <FaExchangeAlt className="md:text-2xl text-yellow-500" />, title: "Real-Time Transactions" },
+    { icon: <FaPercentage className="md:text-2xl text-orange-500" />, title: "Low Conversion Fees" },
+    { icon: <FaClock className="md:text-2xl text-pink-500" />, title: "24/7 Customer Support" },
+    { icon: <FaMobileAlt className="md:text-2xl text-cyan-500" />, title: "Effortless Budgeting" },
+    { icon: <FaBell className="md:text-2xl text-red-500" />, title: "Personalized Alerts" },
+    { icon: <FaChartLine className="md:text-2xl text-indigo-500" />, title: "Digital Innovation" },
+    { icon: <FaMoneyCheckAlt className="md:text-2xl text-teal-500" />, title: "Exclusive Promotions" },
+    { icon: <FaTools className="md:text-2xl text-blueGray-500" />, title: "Integrated Financial Tools" },
+    { icon: <FaLightbulb className="md:text-2xl text-amber-500" />, title: "In-App Education" },
+    { icon: <FaGift className="md:text-2xl text-rose-500" />, title: "Exclusive Promotions" },
+    { icon: <FaMobile className="md:text-2xl text-lightBlue-500" />, title: "Accessibility" },
+    { icon: <FaFileAlt className="md:text-2xl text-orange-500" />, title: "Paperless Statements" },
+    { icon: <FaQuestionCircle className="md:text-2xl text-rose-500" />, title: "In-App Support" },
+  ];
