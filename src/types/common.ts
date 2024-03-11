f0c38e2e54eb8923d86782d7264d3cdbc9d43bf5
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 13:42:00 +0600

    Add interfaces for meta, response error types, and generic response types.

---
+++ src/types/common.ts
@@ -0,0 +1,27 @@
+export interface IMeta {
+  limit: number;
+  page: number;
+  total: number;
+}
+
+export interface IResponseErrorType {
+  statusCode: string;
+  message: string;
+  errorMessages: string;
+}
+
+export type ResponseSuccessType = {
+  data: any;
+  meta?: IMeta;
+};
+
+export type IGenericErrorMessage = {
+  path: string | number;
+  message: string;
+};
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
