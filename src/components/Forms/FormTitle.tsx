744c1f28af82ea420ca0619be9a7e3171796e6dd
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 16:28:56 +0600

    Add FormTitle component 🚀

---
+++ src/components/Forms/FormTitle.tsx
@@ -0,0 +1,9 @@
+const FormTitle = ({ title }: { title: string }) => {
+    return (
+      <h1 className="text-secondary text-xl font-bold text-center">
+        {title}
+      </h1>
+    );
+  };
+  
+  export default FormTitle;
