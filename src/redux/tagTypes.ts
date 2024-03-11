85e893d5843daaf12d9570af8c6ef4c7c31ee10d
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 14:12:38 +0600

    Add enum tagTypes and tagTypesList

---
+++ src/redux/tagTypes.ts
@@ -0,0 +1,13 @@
+export enum tagTypes {
+  auth = "auth",
+  user = "user",
+  admin = "admin",
+  transaction = "transaction",
+}
+
+export const tagTypesList = [
+  tagTypes.auth,
+  tagTypes.user,
+  tagTypes.admin,
+  tagTypes.transaction,
+];
