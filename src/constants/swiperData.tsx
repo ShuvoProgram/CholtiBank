4c9ca031f8e432f6f8f0fdc19ba762b07d255079
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 13:55:28 +0600

    Add images to servicesData

---
+++ src/constants/swiperData.tsx
@@ -0,0 +1,41 @@
+import image1 from "@/assets/swiferCards/trip.jpg"
+import image2 from "@/assets/swiferCards/phone_with_guy.jpg"
+import image3 from "@/assets/swiferCards/mobile_layer.jpg"
+import image4 from "@/assets/swiferCards/reclaim.jpg"
+import image5 from "@/assets/swiferCards/coin.jpg"
+import image6 from "@/assets/swiferCards/protect.jpg"
+import image7 from "@/assets/swiferCards/stay_protected.jpg"
+
+
+export const servicesData = [
+  {
+    id: 1,
+    imageUrl: image1,
+  },
+  {
+    id: 2,
+    imageUrl: image2,
+  },
+
+  {
+    id: 3,
+    imageUrl: image3,
+  },
+
+  {
+    id: 4,
+    imageUrl: image4,
+  },
+  {
+    id: 5,
+    imageUrl: image5,
+  },
+  {
+    id: 6,
+    imageUrl: image6,
+  },
+  {
+    id: 7,
+    imageUrl: image7,
+  },
+];
