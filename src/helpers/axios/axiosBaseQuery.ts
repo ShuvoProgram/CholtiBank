bfadc1e23f4989a6187bf2f91f66cb8c62c40ae0
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 15:59:31 +0600

    Add axiosBaseQuery function

---
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -0,0 +1,44 @@
+import type { BaseQueryFn } from "@reduxjs/toolkit/query";
+import type { AxiosRequestConfig, AxiosError } from "axios";
+import { instance as axiosInstance } from "./axiosInstance";
+import { IMeta } from "@/types/common";
+
+export const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: "" }
+  ): BaseQueryFn<
+    {
+      url: string;
+      method: AxiosRequestConfig["method"];
+      data?: AxiosRequestConfig["data"];
+      params?: AxiosRequestConfig["params"];
+      meta?: IMeta;
+      contentType?: string;
+    },
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data, params, contentType }) => {
+    try {
+      const result = await axiosInstance({
+        url: baseUrl + url,
+        method,
+        data,
+        params,
+        headers: {
+          "Content-Type": contentType || "application/json",
+        },
+        withCredentials: true,
+      });
+
+      return result;
+    } catch (axiosError) {
+      let error = axiosError as AxiosError;
+      return {
+        error: {
+          status: error?.response?.status,
+          data: error?.response?.data || error?.message,
+        },
+      };
+    }
+  };
