95f55e3c75100060b0b829df5c698efa3530be18
ShuvoProgram <shuvoprogramer@gmail.com>
2024-03-11 17:54:28 +0600

    Add Image component for 'Why Choose Us' section 🖼️

---
+++ src/components/Pages/Home/WhyChooseUs/ImagePartWhyChooseUs.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const ImagePartWhyChooseUs = () => {
+  return (
+    <div>
+      <Image
+        src="/homePage/man-with-phone.jpg"
+        width={600}
+        height={700}
+        alt="Why you choose us"
+      />
+    </div>
+  );
+};
+
+export default ImagePartWhyChooseUs;
